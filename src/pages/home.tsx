ab2d9941e0c484cd07992af9ada4612884427f08
erkinov26 <erkinov0247@gmail.com>
2025-05-28 12:28:03 +0500

    icon is changed

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -126,7 +126,11 @@ export function Home() {
                 aria-label="Toggle menu"
               >
                 {
-                  mobileMenuOpen ? <span className="text-white">Close Menu</span> : <span className="text-white">Open Menu</span>
+                  mobileMenuOpen ? <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                  </svg> : <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                  </svg>
                 }
               </button>
             </div>
